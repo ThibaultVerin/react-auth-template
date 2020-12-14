a39419e3070461dcfc69016e20a921d869bd8242
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2020-12-14 23:28:25 +0100

    cleanup

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,12 +2,18 @@ import { useQuery } from 'react-query';
 import API from '../services/API';
 
 export default function Profile() {
-  const { data: profile } = useQuery('me', () => API.get('/me'));
+  const { data: profile } = useQuery('me', () =>
+    API.get('/me').then((res) => res.data)
+  );
   if (!profile) return <p>Loading profile...</p>;
 
   const logout = async () => {
-    console.log('logging out');
-    // TODO
+    try {
+      await API.get('/auth/logout');
+      console.log('logged out successfully');
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
